a3602c982b13df9c9973b19917ae72d42cfa4220
Kinozinhoo <162368756+Kinozinhoo@users.noreply.github.com>
2024-03-12 16:23:16 -0300

    Add files via upload

---
+++ Aula03/sla2.js
@@ -0,0 +1,10 @@
+let meuNome = null
+let minhaIdade = null
+
+console.log (typeof meuNome, typeof minhaIdade)
+//ela ficou objeto nulo porque a viriavel é nula
+meuNome = 'Liuji'
+minhaIdade = 15
+
+console.log ("Olá", meuNome , 'você tem',
+minhaIdade, )
